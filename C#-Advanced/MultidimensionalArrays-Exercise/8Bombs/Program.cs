44bfde77c426dad47a6b833f1e87c711cb382118
angeldragiew <angeldragieww@gmail.com>
2021-01-26 01:51:18 +0200

    Added new Exercises to MultidimensionalArrays-Exercise

---
+++ C#-Advanced/MultidimensionalArrays-Exercise/8Bombs/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _8Bombs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+
+            int[,] field = new int[size, size];
+
+            for (int row = 0; row < size; row++)
+            {
+                int[] rowData = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                for (int col = 0; col < size; col++)
+                {
+                    field[row, col] = rowData[col];
+                }
+            }
+
+            string[] bombsCordinates = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+            for (int i = 0; i < bombsCordinates.Length; i++)
+            {
+                string[] cordinates = bombsCordinates[i].Split(',', StringSplitOptions.RemoveEmptyEntries).ToArray();
+                int row = int.Parse(cordinates[0]);
+                int col = int.Parse(cordinates[1]);
+                Explode(row, col, field);
+
+            }
+
+            int sum = 0;
+            int activeCells = 0;
+
+            foreach (var item in field)
+            {
+                if (item > 0)
+                {
+                    sum += item;
+                    activeCells++;
+                }
+            }
+
+            Console.WriteLine($"Alive cells: {activeCells}");
+            Console.WriteLine($"Sum: {sum}");
+            for (int row = 0; row < field.GetLength(0); row++)
+            {
+                for (int col = 0; col < field.GetLength(1); col++)
+                {
+                    Console.Write($"{field[row, col]} ");
+
+                }
+                Console.WriteLine();
+            }
+
+        }
+
+        public static void Explode(int row, int col, int[,] field)
+        {
+            int power = field[row, col];
+
+            for (int currRow = row - 1; currRow <= row + 1; currRow++)
+            {
+                for (int currCol = col - 1; currCol <= col + 1; currCol++)
+                {
+                    if (currRow >= 0 && currRow < field.GetLength(0) && currCol >= 0 && currCol < field.GetLength(1))
+                    {
+                        if (field[currRow, currCol] <= 0 || power < 0)
+                        {
+                            continue;
+                        }
+                        field[currRow, currCol] -= power;
+                    }
+
+                }
+            }
+        }
+    }
+}
---
+++ C#-Advanced/MultidimensionalArrays-Exercise/9Miner/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Linq;
+
+namespace _9Miner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+            char[,] field = new char[size, size];
+
+            string[] directions = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+            for (int row = 0; row < size; row++)
+            {
+                char[] rowData = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(char.Parse).ToArray();
+                for (int col = 0; col < size; col++)
+                {
+                    field[row, col] = rowData[col];
+                }
+            }
+            int coals = CoalsCount(field);
+            int[] startCoordinates = FindStartCordinates(field);
+            int currRow = startCoordinates[0];
+            int currCol = startCoordinates[1];
+            bool end = false;
+            foreach (var direction in directions)
+            {
+                if (direction == "left" && ValidateCoordinates(currRow, currCol - 1, field))
+                {
+                    currCol -= 1;
+                }
+                else if (direction == "right" && ValidateCoordinates(currRow, currCol + 1, field))
+                {
+                    currCol += 1;
+                }
+                else if (direction == "up" && ValidateCoordinates(currRow - 1, currCol, field))
+                {
+                    currRow -= 1;
+                }
+                else if (direction == "down" && ValidateCoordinates(currRow + 1, currCol, field))
+                {
+                    currRow += 1;
+                }
+
+                if (field[currRow, currCol] == 'c')
+                {
+                    coals--;
+                    field[currRow, currCol] = '*';
+                }
+                else if (field[currRow, currCol] == 'e')
+                {
+                    end = true;
+                }
+            }
+
+            if (end)
+            {
+                Console.WriteLine($"Game over! {(currRow, currCol)}");
+            }
+            else if (coals > 0)
+            {
+                Console.WriteLine($"{coals} coals left. ({currRow}, {currCol})");
+            }
+            else if (coals == 0)
+            {
+                Console.WriteLine($"You collected all coals! ({currRow}, {currCol})");
+            }
+
+        }
+
+        public static int[] FindStartCordinates(char[,] field)
+        {
+            int[] startCoordinates = new int[2];
+            for (int row = 0; row < field.GetLength(0); row++)
+            {
+                for (int col = 0; col < field.GetLength(1); col++)
+                {
+                    if (field[row, col] == 's')
+                    {
+                        startCoordinates[0] = row;
+                        startCoordinates[1] = col;
+                    }
+                }
+            }
+            return startCoordinates;
+        }
+
+        public static bool ValidateCoordinates(int row, int col, char[,] field)
+        {
+            bool isValid = false;
+
+            if (row >= 0 && row < field.GetLength(0) && col >= 0 && col < field.GetLength(1))
+            {
+                isValid = true;
+            }
+
+            return isValid;
+        }
+
+        public static int CoalsCount(char[,] field)
+        {
+            int counter = 0;
+            foreach (var item in field)
+            {
+                if (item == 'c')
+                {
+                    counter++;
+                }
+            }
+            return counter;
+        }
+        public static int isCoalOrEnd(int row, int col, char[,] field, int coals)
+        {
+            if (field[row, col] == 'c')
+            {
+                coals--;
+                return coals;
+            }
+            else if (field[row, col] == 'e')
+            {
+                return -1;
+            }
+            return coals;
+        }
+
+    }
+}
