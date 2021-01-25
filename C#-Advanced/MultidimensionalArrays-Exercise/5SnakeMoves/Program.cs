a9352ccaaaa8b09a7160ea93cc44753ab6c1dcf2
angeldragiew <angeldragieww@gmail.com>
2021-01-25 19:48:49 +0200

    Added 5SnakeMoves

--- C#-Advanced/MultidimensionalArrays-Exercise/5SnakeMoves/Program.cs
+++ C#-Advanced/MultidimensionalArrays-Exercise/5SnakeMoves/Program.cs
@@ -17,7 +17,6 @@ namespace _5SnakeMoves
             string wordInput = Console.ReadLine();
             Queue<char> word = new Queue<char>(wordInput);
 
-
             for (int row = 0; row < matrix.GetLength(0); row++)
             {
                 if (row % 2 == 0)
