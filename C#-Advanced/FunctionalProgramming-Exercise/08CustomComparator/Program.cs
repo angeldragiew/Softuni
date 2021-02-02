abab3623675378cc1fba690d3fce304010923ba9
angeldragiew <angeldragieww@gmail.com>
2021-02-03 01:11:53 +0200

    Added more exercises to FunctionalProgramming-Exercise

---
+++ C#-Advanced/FunctionalProgramming-Exercise/08CustomComparator/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace _08CustomComparator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Array.Sort(numbers, (int a, int b) =>
+            {
+                if (a % 2 != 0 && b % 2 == 0)
+                {
+                    return 1;
+                }
+                else if (a % 2 == 0 && b % 2 != 0)
+                {
+                    return -1;
+                }
+                return a.CompareTo(b);
+                return 0;
+            });
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
---
+++ C#-Advanced/FunctionalProgramming-Exercise/09ListOfPredicates/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _09ListOfPredicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int end = int.Parse(Console.ReadLine());
+            List<int> dividers = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            Func<int, List<int>, bool> divisibleByAll = (n, dividers) =>
+            {
+                if (dividers.All(d => n % d == 0))
+                {
+                    return true;
+                }
+                return false;
+            };
+
+            Func<int, List<int>> fillList = end =>
+              {
+                  List<int> list = new List<int>();
+                  for (int i = 0; i < end; i++)
+                  {
+                      int currNum = i + 1;
+                      if (divisibleByAll(currNum, dividers))
+                      {
+                          list.Add(currNum);
+                      }
+                  }
+                  return list;
+              };
+
+            List<int> numbers = fillList(end);
+            Console.WriteLine(string.Join(" ", numbers));
+
+        }
+    }
+}
