264edc4bf1cfd767c0bbb95ea82c9d1609888e64
angeldragiew <angeldragieww@gmail.com>
2021-02-05 00:49:51 +0200

    Added DefiningClasses-Exercise

---
+++ C#-Advanced/DefiningClasses-Exercise/01DefineAClassPerson/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Person Angel = new Person();
+            Angel.Name = "Angel";
+            Angel.Age = 94;
+
+            Person Ivan = new Person() { Name = "Ivan", Age = 99 };
+            
+        }
+    }
+}
---
+++ C#-Advanced/DefiningClasses-Exercise/02CreatingConstructors/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Person firstPerson = new Person();
+            Person secondPerson = new Person(12);
+            Person thirdPerson = new Person("Gogi", 38);
+            ;
+        }
+    }
+}
