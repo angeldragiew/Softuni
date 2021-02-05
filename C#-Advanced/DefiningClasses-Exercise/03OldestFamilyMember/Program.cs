e46acf66f6bad97c6bdad20c91b2a6f5ef65032f
angeldragiew <angeldragieww@gmail.com>
2021-02-05 14:07:51 +0200

    Added more exercises to DefiningClasses-Exercise

---
+++ C#-Advanced/DefiningClasses-Exercise/03OldestFamilyMember/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Family family = new Family();
+            for (int i = 0; i < n; i++)
+            {
+                string[] personInfo = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string name = personInfo[0];
+                int age = int.Parse(personInfo[1]);
+                Person currPerson = new Person(name, age);
+                family.AddMember(currPerson);
+            }
+
+            Person oldestPerson = family.GetOldestGetOldestMember();
+            Console.WriteLine($"{oldestPerson.Name} {oldestPerson.Age}");
+        }
+    }
+}
---
+++ C#-Advanced/DefiningClasses-Exercise/04OpinionPoll/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<Person> people = new List<Person>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] personInfo = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string name = personInfo[0];
+                int age = int.Parse(personInfo[1]);
+
+                Person currPerson = new Person(name, age);
+                people.Add(currPerson);
+            }
+
+            foreach (var person in people.Where(p=>p.Age>30).OrderBy(p=>p.Name))
+            {
+                Console.WriteLine($"{person.Name} - {person.Age}");
+            }
+        }
+    }
+}
