39edcfff48aa7bfa8ffd69d53baa8145c26620ed
angeldragiew <angeldragieww@gmail.com>
2021-02-10 20:28:00 +0200

    Added ImplementingList

---
+++ C#-Advanced/ImplementingList/ImplementingList/CustomList.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImplementingList
+{
+    class CustomList
+    {
+        private const int InitialCapacity = 2;
+        private int[] array;
+
+        public CustomList()
+        {
+            array = new int[InitialCapacity];
+        }
+
+        public int Count { get; private set; }
+
+        public int this[int index]
+        {
+            get
+            {
+                ValidateIndex(index);
+                return array[index];
+            }
+            set
+            {
+                ValidateIndex(index);
+                array[index] = value;
+            }
+        }
+
+        public void Add(int n)
+        {
+            if (Count == array.Length)
+            {
+                Resize();
+            }
+            array[Count] = n;
+            Count++;
+        }
+
+        public int RemoveAt(int index)
+        {
+            ValidateIndex(index);
+            int itemToRemove = array[index];
+            array[index] = default;
+            Shift(index);
+            Count--;
+
+            if (array.Length / 4 == Count)
+            {
+                Shrink();
+            }
+
+            return itemToRemove;
+        }
+        public void Insert(int index, int item)
+        {
+            if (index > Count)
+            {
+                throw new IndexOutOfRangeException("Invalid index");
+            }
+            if (Count == array.Length)
+            {
+                Resize();
+            }
+            ShiftRight(index);
+            array[index] = item;
+            Count++;
+        }
+
+        public bool Contains(int item)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                if (array[i] == item)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void Swap(int firstIndex, int secondIndex)
+        {
+            ValidateIndex(firstIndex);
+            ValidateIndex(secondIndex);
+
+            int firstIndexValue = array[firstIndex];
+            array[firstIndex] = array[secondIndex];
+            array[secondIndex] = firstIndexValue;
+        }
+
+        private void Resize()
+        {
+            int[] resizedArray = new int[array.Length * 2];
+            Array.Copy(array, resizedArray, Count);
+            array = resizedArray;
+        }
+
+        private void Shrink()
+        {
+            int[] shrinkedArray = new int[array.Length / 2];
+            Array.Copy(array, shrinkedArray, Count);
+            array = shrinkedArray;
+        }
+
+        private void Shift(int index)
+        {
+            for (int i = index; i < Count - 1; i++)
+            {
+                array[i] = array[i + 1];
+            }
+            array[Count - 1] = default;
+        }
+
+        private void ShiftRight(int index)
+        {
+            for (int i = Count; i > index; i--)
+            {
+                array[i] = array[i - 1];
+            }
+        }
+
+        private void ValidateIndex(int index)
+        {
+            if (index >= Count)
+            {
+                throw new IndexOutOfRangeException("Invalid index");
+            }
+        }
+    }
+}
---
+++ C#-Advanced/ImplementingList/ImplementingList/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ImplementingList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomList list = new CustomList();
+
+            list.Add(399);
+            list.Add(200);
+            list.Add(200);
+            list.Add(200);
+            list.Insert(4, 13);
+            Console.WriteLine("removing " + list.RemoveAt(3));
+            Console.WriteLine("removing " + list.RemoveAt(2));
+            Console.WriteLine("removing " + list.RemoveAt(1));
+            Console.WriteLine(list[0]);
+            Console.WriteLine(list[1]);
+            Console.WriteLine(list.Contains(13));
+
+            list.Swap(0, 1);
+            Console.WriteLine(list[0]);
+            Console.WriteLine(list[1]);
+        }
+    }
+}
