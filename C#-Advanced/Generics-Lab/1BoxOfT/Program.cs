f450e52524bff1499aa116af15de1c5d77efe6b5
angeldragiew <angeldragieww@gmail.com>
2021-02-11 19:44:39 +0200

    Added Generics-Lab

---
+++ C#-Advanced/Generics-Lab/1BoxOfT/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace BoxOfT
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Box<int> box = new Box<int>();
+            box.Add(1);
+            box.Add(2);
+            box.Add(3);
+
+            Console.WriteLine(box.Count);
+
+            Console.WriteLine("Removing " + box.Remove());
+            Console.WriteLine("Removing " + box.Remove());
+
+            Console.WriteLine(box.Count);
+        }
+    }
+}
---
+++ C#-Advanced/Generics-Lab/2ArrayCreator/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GenericArrayCreator
+{
+   public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int[] array = ArrayCreator.Create(5, 2);
+
+            Console.WriteLine(string.Join(" ", array));
+        }
+    }
+}
