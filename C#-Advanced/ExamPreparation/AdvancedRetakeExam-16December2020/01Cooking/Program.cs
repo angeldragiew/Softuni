f88c3b40fc188885204f2b4bf7f4dd80aeeb3fad
angeldragiew <angeldragieww@gmail.com>
2021-02-18 21:35:40 +0200

    Added AdvancedRetakeExam-16December2020

---
+++ C#-Advanced/ExamPreparation/AdvancedRetakeExam-16December2020/01Cooking/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01Cooking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> foods = new Dictionary<string, int>()
+            {
+                {"Bread", 0 },
+                {"Cake", 0 },
+                {"Pastry", 0 },
+                {"Fruit Pie", 0 }
+            };
+            int[] liquidInfo = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] ingredientsInfo = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Queue<int> liquids = new Queue<int>(liquidInfo);
+            Stack<int> ingredients = new Stack<int>(ingredientsInfo);
+
+            while (liquids.Any() && ingredients.Any())
+            {
+                int currLiquid = liquids.Dequeue();
+                int currIngredient = ingredients.Pop();
+
+                int sum = currIngredient + currLiquid;
+                string foodToAdd = string.Empty;
+                if (sum == 25)
+                {
+                    foodToAdd = "Bread";
+                }
+                else if (sum == 50)
+                {
+                    foodToAdd = "Cake";
+                }
+                else if (sum == 75)
+                {
+                    foodToAdd = "Pastry";
+                }
+                else if (sum == 100)
+                {
+                    foodToAdd = "Fruit Pie";
+                }
+                else
+                {
+                    currIngredient += 3;
+                    ingredients.Push(currIngredient);
+                    continue;
+                }
+
+                foods[foodToAdd]++;
+            }
+
+            bool isCookedEverything = true;
+            foreach (var food in foods)
+            {
+                if (food.Value == 0)
+                {
+                    isCookedEverything = false;
+                    break;
+                }
+            }
+
+            if (isCookedEverything)
+            {
+                Console.WriteLine("Wohoo! You succeeded in cooking all the food!");
+            }
+            else
+            {
+                Console.WriteLine("Ugh, what a pity! You didn't have enough materials to cook everything.");
+            }
+
+            if (liquids.Any())
+            {
+                Console.WriteLine($"Liquids left: {string.Join(", ", liquids)}");
+            }
+            else
+            {
+                Console.WriteLine("Liquids left: none");
+            }
+
+            if (ingredients.Any())
+            {
+                Console.WriteLine($"Ingredients left: {string.Join(", ", ingredients)}");
+            }
+            else
+            {
+                Console.WriteLine("Ingredients left: none");
+            }
+
+            foreach (var food in foods.OrderBy(f => f.Key))
+            {
+                Console.WriteLine($"{food.Key}: {food.Value}");
+            }
+        }
+    }
+}
---
+++ C#-Advanced/ExamPreparation/AdvancedRetakeExam-16December2020/02Selling/Program.cs
@@ -0,0 +1,156 @@
+﻿using System;
+
+namespace _02Selling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            char[,] field = new char[n, n];
+
+            int currRow = 0;
+            int currCol = 0;
+            bool hasPillars = false;
+            for (int row = 0; row < n; row++)
+            {
+                string rowData = Console.ReadLine();
+                for (int col = 0; col < n; col++)
+                {
+                    field[row, col] = rowData[col];
+                    if (field[row, col] == 'S')
+                    {
+                        currRow = row;
+                        currCol = col;
+                    }
+                    if (field[row, col] == 'O')
+                    {
+                        hasPillars = true;
+                    }
+                }
+            }
+
+            int[] pillarsCoordinates = new int[4];
+            int index = 0;
+            if (hasPillars)
+            {
+                for (int row = 0; row < n; row++)
+                {
+                    for (int col = 0; col < n; col++)
+                    {
+                        if (field[row, col] == 'O')
+                        {
+                            pillarsCoordinates[index] = row;
+                            pillarsCoordinates[index + 1] = col;
+                            index += 2;
+                        }
+                    }
+                }
+            }
+
+            int money = 0;
+
+            bool hasCollectedEnoughMoney = true;
+            while (money < 50)
+            {
+                string direction = Console.ReadLine();
+                field[currRow, currCol] = '-';
+                currRow = MoveRow(currRow, direction);
+                currCol = MoveCol(currCol, direction);
+                if(IsPositionValid(currRow, currCol, n, n) == false)
+                {
+                    hasCollectedEnoughMoney = false;
+                    Console.WriteLine("Bad news, you are out of the bakery.");
+                    break;
+                }
+                else if (field[currRow, currCol] == 'O')
+                {
+                    int firstPillarRow = pillarsCoordinates[0];
+                    int firstPillarCol = pillarsCoordinates[1];
+                    int secondPillarRow = pillarsCoordinates[2];
+                    int secondPillarCol = pillarsCoordinates[3];
+
+                    field[currRow, currCol] = '-';
+                    if (currRow == firstPillarRow && currCol == firstPillarCol)
+                    {
+                        currRow = secondPillarRow;
+                        currCol = secondPillarCol;
+                    }
+                    else
+                    {
+                        currRow = firstPillarRow;
+                        currCol = firstPillarCol;
+                    }
+                }
+                else if (field[currRow, currCol] == '-')
+                {
+                    field[currRow, currCol] = 'S';
+                    continue;
+                }
+                else
+                {
+                    money += int.Parse(field[currRow, currCol].ToString());
+                }
+
+                field[currRow, currCol] = 'S';
+            }
+
+            if (hasCollectedEnoughMoney)
+            {
+                Console.WriteLine("Good news! You succeeded in collecting enough money!");
+            }
+            Console.WriteLine($"Money: {money}");
+
+            for (int row = 0; row < n; row++)
+            {
+                for (int col = 0; col < n; col++)
+                {
+                    Console.Write(field[row,col]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public static bool IsPositionValid(int row, int col, int rows, int cols)
+        {
+            if (row < 0 || row >= rows)
+            {
+                return false;
+            }
+            if (col < 0 || col >= cols)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public static int MoveRow(int row, string movement)
+        {
+            if (movement == "up")
+            {
+                return row - 1;
+            }
+            if (movement == "down")
+            {
+                return row + 1;
+            }
+
+            return row;
+        }
+
+        public static int MoveCol(int col, string movement)
+        {
+            if (movement == "left")
+            {
+                return col - 1;
+            }
+            if (movement == "right")
+            {
+                return col + 1;
+            }
+
+            return col;
+        }
+    }
+}
