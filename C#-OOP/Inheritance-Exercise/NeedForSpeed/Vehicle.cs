b346c21aa282cb7fd71d60d273288e00e4d798c3
angeldragiew <angeldragieww@gmail.com>
2021-03-02 21:13:27 +0200

    Added more exercises to Inheritance-Exercise

---
+++ C#-OOP/Inheritance-Exercise/NeedForSpeed/Vehicle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeedForSpeed
+{
+    public class Vehicle
+    {
+        private const double defaultFuelConsumption = 1.25;
+        public Vehicle(int horsePower, double fuel)
+        {
+            HorsePower = horsePower;
+            Fuel = fuel;
+        }
+
+        public virtual double FuelConsumption => defaultFuelConsumption;
+        public int HorsePower { get; set; }
+
+        public double Fuel { get; set; }
+
+        public virtual void Drive(double kilometers)
+        {
+            if (Fuel - kilometers * FuelConsumption >= 0)
+            {
+                Fuel -= kilometers * FuelConsumption;
+
+            }
+        }
+    }
+}
---
+++ C#-OOP/Inheritance-Exercise/Restaurant/Coffee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public class Coffee : HotBeverage
+    {
+        private const decimal price = 5M;
+        private const double millilitres = 50;
+        public Coffee(string name, double caffeine) 
+            : base(name, price, millilitres)
+        {
+            Caffeine = caffeine;
+        }
+        public double Caffeine  { get; set; }
+    }
+}
