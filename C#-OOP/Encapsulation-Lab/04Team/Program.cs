cf25918bcd032f47692d999088f886f5bc851a1a
angeldragiew <angeldragieww@gmail.com>
2021-03-04 21:38:34 +0200

    Added more exercises to Encapsulation-Lab

---
+++ C#-OOP/Encapsulation-Lab/04Team/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PersonsInfo
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+
+            Team team = new Team("A");
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] personInfo = Console.ReadLine().Split();
+
+                decimal salary = decimal.Parse(personInfo[3]);
+                Person person = new Person(personInfo[0], personInfo[1], int.Parse(personInfo[2]), salary);
+                team.AddPlayer(person);
+            }
+
+            Console.WriteLine($"First team has {team.FirstTeam.Count} players.");
+            Console.WriteLine($"Reserve team has {team.ReserveTeam.Count} players.");
+
+           
+        }
+    }
+}
