6639381d77d2e25b9895274eeef09ed6acac237f
angeldragiew <angeldragieww@gmail.com>
2021-03-09 08:27:58 +0200

    Added more exercises to InterfacesAndAbstraction-Lab

--- C#-OOP/InterfacesAndAbstraction-Lab/02Cars/Program.cs
+++ C#-OOP/InterfacesAndAbstraction-Lab/02Cars/Program.cs
@@ -6,7 +6,12 @@ namespace Cars
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ICar seat = new Seat("Leon", "Grey");
+            ICar tesla = new Tesla("Model 3", "Red", 2);
+
+            Console.WriteLine(seat.ToString());
+            Console.WriteLine(tesla.ToString());
+
         }
     }
 }
