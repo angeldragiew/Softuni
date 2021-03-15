49b2eca0762b4b4a488bd28317c9d9fb11e31f61
angeldragiew <angeldragieww@gmail.com>
2021-03-15 19:02:55 +0200

    Added Polymorphism-Lab

---
+++ C#-OOP/Polymorphism-Lab/03Shapes/Rectangle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public class Rectangle : Shape
+    {
+        private double h;
+        private double w;
+
+        public Rectangle(double height, double width)
+        {
+            h = height;
+            w = width;
+        }
+
+        public override double CalculateArea()
+        {
+            return h * w;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return 2 * (h + w);
+        }
+
+        public override string Draw()
+        {
+            return $"Drawing {nameof(Rectangle)}";
+        }
+    }
+}
