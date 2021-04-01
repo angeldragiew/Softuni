374be3112982a173098d695b1ceeae0d726e2fd5
angeldragiew <angeldragieww@gmail.com>
2021-04-01 17:39:03 +0300

    Added MockingAndTestDrivenDevelopment-Lab

---
+++ C#-OOP/MockingAndTestDrivenDevelopment-Lab/FakeAxeAndDummy.Tests/HeroTests.cs
@@ -0,0 +1,23 @@
+﻿using FakeAxeAndDummy.Contracts;
+using Moq;
+using NUnit.Framework;
+
+[TestFixture]
+public class HeroTests
+{
+    [Test]
+    public void HeroShouldGainXP_When_TargetDies()
+    {
+        Mock<ITarget> target = new Mock<ITarget>();
+        Mock<IWeapon> weapon = new Mock<IWeapon>();
+
+        target.Setup(p => p.IsDead()).Returns(true);
+        target.Setup(p => p.GiveExperience()).Returns(20);
+
+        Hero hero = new Hero("Angel", weapon.Object);
+
+        hero.Attack(target.Object);
+
+        Assert.That(hero.Experience, Is.EqualTo(20));
+    }
+}
---
+++ C#-OOP/MockingAndTestDrivenDevelopment-Lab/FakeAxeAndDummy/Contracts/ITarget.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FakeAxeAndDummy.Contracts
+{
+    public interface ITarget
+    {
+        bool IsDead();
+
+        int GiveExperience();
+
+        void TakeAttack(int attackPoints);
+    }
+}
