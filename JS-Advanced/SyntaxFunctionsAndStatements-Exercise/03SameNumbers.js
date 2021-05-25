41eb9eba4ba55ff9bc2a198194316dfa127b594f
angeldragiew <angeldragieww@gmail.com>
2021-05-26 00:16:06 +0300

    Added SyntaxFunctionsAndStatements-Exercise

---
+++ JS-Advanced/SyntaxFunctionsAndStatements-Exercise/03SameNumbers.js
@@ -0,0 +1,17 @@
+function solve(number) {
+    let numbersArray = String(number).split('').map(x=>+x);
+
+    let areSame = true;
+    for (let i = 0; i < numbersArray.length - 1; i++) {
+        if (numbersArray[i] !== numbersArray[i + 1]) {
+            areSame = false;
+            break;
+        }
+    }
+
+    console.log(areSame);
+    console.log(numbersArray.reduce((a, b) => a + b, 0));
+}
+
+solve(2222222);
+solve(1234);
